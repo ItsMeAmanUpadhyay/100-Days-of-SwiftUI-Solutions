6008c3b6500ad2aa9d5bceb039c3ca6082f06686
Aman <71437805+ItsMeAmanUpadhyay@users.noreply.github.com>
2022-06-30 00:13:09 +0530

    Add files via upload

---
+++ Checkpoint_1_SolutionCode/Checkpoint_1_SolutionCode.playground/Contents.swift
@@ -0,0 +1,3 @@
+let tempInC = 0.0 // Constant created conatining Temp in Celsius
+let tempInF = tempInC * 9 / 5 + 32 // Conversion of Temp from Celsius to Fahrenheit
+print("Temperature is \(tempInC)°C and \(tempInF)°F") // Printing both the temp using string interpolation
